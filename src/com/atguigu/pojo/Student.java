390ecba9765ea33de89221f8e355c8a629989d2b
wuzhaowen <513956702@qq.com>
2019-02-21 16:34:14 +0800

    mamlageb

---
+++ src/com/atguigu/pojo/Student.java
@@ -0,0 +1,25 @@
+package com.atguigu.pojo;
+
+public class Student {
+	String name;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
